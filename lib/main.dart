c70e71eda2497eaab1343572733a611dce1e8371
RubenAlfandoWhite <rubenalfandowhite_2428250004@mhs.mdp.ac.id>
2026-03-04 14:48:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/screens/home_screen.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pilem',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
         ),
-      home: const HomeScreen()
+      home: const MainScreen()
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+      const HomeScreen(),
+      const Placeholder(),
+      const Placeholder(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+      ),
     );
   }
 }
